25fdedf251d53026131886cbef1ac935f5e3bf7c
abnilson marcolino <nilmarcolino@hotmail.com>
2025-03-18 13:09:24 -0300

    Add files via upload

--- Aula1.c
+++ Aula1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    printf("Alo Mundo\n");
     
-    return 0;
+    int idade = 18;
+    int resultado;
+
+resultado = idade >= 18 ? 1 : 0;
+
+if (resultado == 1) {
+    printf("Maior de idade\n");
+} else {
+    printf("Menor de idade\n");
+
+
+    return 0;   
+} 
 }
